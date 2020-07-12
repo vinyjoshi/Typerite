a0ddbe74559f1ca6ab0722f73523b633449e74bb
Vinay Joshi <viyj2000@gmail.com>
2020-07-12 22:59:45 +0530

    VideoField

--- Category/models.py
+++ Category/models.py
@@ -7,6 +7,10 @@ class Blog(models.Model):
     date = models.CharField(max_length=200)    
     time = models.CharField(max_length=200)
     content = models.TextField(blank=False)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    video = models.FileField(upload_to='videos/%Y/%m/%d/', blank=True)
     
     def __str__(self):
         return self.title
