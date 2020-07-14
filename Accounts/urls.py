20cd228581114dad9205576f543616177aa64384
Vinay Joshi <viyj2000@gmail.com>
2020-07-14 12:37:05 +0530

    Basic

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Login', views.Login, name='Login'),
-    path('Register', views.Register, name='Register'),    
-    path('Timeline', views.Timeline, name='Timeline'),    
+    path('Login/', views.Login, name='Login'),
+    path('Register/', views.Register, name='Register'),    
+    path('Timeline/', views.Timeline, name='Timeline'),    
 ]   
