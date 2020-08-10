32173b89ff009c2e8794344f01c396df5b2f5c8d
Vinay Joshi <60395756+vinyjoshi@users.noreply.github.com>
2020-08-10 12:03:42 +0530

    Update settings.py

--- typerite/settings.py
+++ typerite/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'typeritedb',
         'USER': 'viny',
-        'PASSWORD': 'Inazuma11',
+        'PASSWORD': '***********',
         'HOST': 'localhost',
     }
 }
@@ -130,5 +130,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'viyj1995@gmail.com'
-EMAIL_HOST_PASSWORD = 'Inazuma11'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_USE_TLS = True
